9a19ca40dcbda3f7307254aa46f3898f30fcc209
Aarni Fagerström <aarni.fagerstrom@gmail.com>
2023-12-31 17:48:43 +0200

    Tehtavat 13.14

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -37,9 +37,18 @@ router.get("/", async (req, res) => {
       attributes: ["name"],
     },
     where: {
-      title: {
-        [Op.iLike]: req.query.search ? `%${req.query.search}%` : `%${""}%`,
-      },
+      [Op.or]: [
+        {
+          title: {
+            [Op.iLike]: req.query.search ? `%${req.query.search}%` : `%${""}%`,
+          },
+        },
+        {
+          author: {
+            [Op.iLike]: req.query.search ? `%${req.query.search}%` : `%${""}%`,
+          },
+        },
+      ],
     },
   });
   res.json(blogs);
