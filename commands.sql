a6c82cf617e762b5fa85e12a418a4c66ff4ddb77
Aarni Fagerström <aarni.fagerstrom@gmail.com>
2024-01-01 17:21:13 +0200

    Tehtava 13.19

--- commands.sql
+++ commands.sql
@@ -9,7 +9,15 @@ CREATE TABLE blogs (
 CREATE TABLE users (
     id SERIAL PRIMARY KEY,
     name text NOT NULL,
-    username text NOT NULL,
+    username text NOT NULL
+);
+
+CREATE TABLE reading_list (
+    id SERIAL PRIMARY KEY,
+    user_id integer NOT NULL,
+    blog_id integer NOT NULL,
+    foreign key (user_id) references users(user_id),
+    foreign key (blog_id) references blogs(blog_id)
 );
 
 insert into users (name, username, created_at, updated_at) values ('John Doe', 'johnD@gmail.com', current_timestamp, current_timestamp);
@@ -21,3 +29,8 @@ insert into blogs (author, url, title, year, likes, created_at, updated_at, user
 insert into blogs (author, url, title, year, likes, created_at, updated_at, user_id) values ('Robert C. Martin', 'http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll', 'First class tests', 2021, 10, current_timestamp, current_timestamp, 2);
 insert into blogs (author, url, title, year, likes, created_at, updated_at, user_id) values ('Robert C. Martin', 'http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html', 'TDD harms architecture', 2009, 0, current_timestamp, current_timestamp, 2);
 insert into blogs (author, url, title, year, likes, created_at, updated_at, user_id) values ('Robert C. Martin', 'http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html', 'Type wars', 2010, 2, current_timestamp, current_timestamp, 2);
+
+insert into reading_list (user_id, blog_id) values (1, 2);
+insert into reading_list (user_id, blog_id) values (1, 3);
+insert into reading_list (user_id, blog_id) values (2, 3);
+insert into reading_list (user_id, blog_id) values (2, 4);
