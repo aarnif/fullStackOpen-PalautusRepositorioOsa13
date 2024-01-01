a4b258be3d7310a5c0861be82349ecb515f34084
Aarni Fagerström <aarni.fagerstrom@gmail.com>
2024-01-01 11:25:29 +0200

    Tehtava 13.17

--- commands.sql
+++ commands.sql
@@ -12,12 +12,12 @@ CREATE TABLE users (
     username text NOT NULL,
 );
 
-insert into blogs (author, url, title, likes) values ('Michael Chan', 'https://reactpatterns.com/', 'React patterns', 7);
-insert into blogs (author, url, title, likes) values ('Edsger W. Dijkstra', 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html', 'Go To Statement Considered Harmful', 5);
-insert into blogs (author, url, title, likes) values ('Edsger W. Dijkstra', 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html', 'Canonical string reduction', 12);
-insert into blogs (author, url, title, likes) values ('Robert C. Martin', 'http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll', 'First class tests', 10);
-insert into blogs (author, url, title, likes) values ('Robert C. Martin', 'http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html', 'TDD harms architecture', 0);
-insert into blogs (author, url, title, likes) values ('Robert C. Martin', 'http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html', 'Type wars', 2);
+insert into blogs (author, url, title, likes, created_at, updated_at, user_id) values ('Michael Chan', 'https://reactpatterns.com/', 'React patterns', 7, current_timestamp, current_timestamp, 1);
+insert into blogs (author, url, title, likes, created_at, updated_at, user_id) values ('Edsger W. Dijkstra', 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html', 'Go To Statement Considered Harmful', 5, current_timestamp, current_timestamp, 1);
+insert into blogs (author, url, title, likes, created_at, updated_at, user_id) values ('Edsger W. Dijkstra', 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html', 'Canonical string reduction', 12, current_timestamp, current_timestamp, 1);
+insert into blogs (author, url, title, likes, created_at, updated_at, user_id) values ('Robert C. Martin', 'http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll', 'First class tests', 10, current_timestamp, current_timestamp, 2);
+insert into blogs (author, url, title, likes, created_at, updated_at, user_id) values ('Robert C. Martin', 'http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html', 'TDD harms architecture', 0, current_timestamp, current_timestamp, 2);
+insert into blogs (author, url, title, likes, created_at, updated_at, user_id) values ('Robert C. Martin', 'http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html', 'Type wars', 2, current_timestamp, current_timestamp, 2);
 
 insert into users (name, username, created_at, updated_at) values ('John Doe', 'johnD@gmail.com', current_timestamp, current_timestamp);
 insert into users (name, username, created_at, updated_at) values ('Jane Doe', 'janeD@gmail.com', current_timestamp, current_timestamp);
